75aa00a7762ad39af45596ec2372810b88c904e3
immyj <johnsonimmy@gmail.com>
2024-11-13 21:36:09 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("workoutForm");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = {
+        exercise: form.exercise.value,
+        sets: form.sets.value,
+        reps: form.reps.value,
+        weight: form.weight.value,
+    };
+
+    fetch("https://script.google.com/macros/s/AKfycbz27wpoNqE-4VCfRhIQrsiPiU1dFsrjGDz2oOIZhwdSZywufyqw7v4E7SXvVsOYjl18SA/execT_URL", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+    })
+        .then((response) => response.text())
+        .then((data) => alert("Workout saved!"))
+        .catch((error) => console.error("Error:", error));
+});
