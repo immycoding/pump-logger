7757fe534883dfe139813a4b154cb21ba72d2b37
immyj <johnsonimmy@gmail.com>
2024-11-20 21:10:00 -0600

    closed brackets

--- script.js
+++ script.js
@@ -26,28 +26,32 @@ if (window.location.pathname.endsWith("workout.html")) {
             option.textContent = exercise;
             dropdown.appendChild(option);
         });
-    }}
-        const form = document.getElementById("workoutForm");
-        const API_KEY = "noscammerspls";
+    }
+}
 
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
+const form = document.getElementById("workoutForm");
+const API_KEY = "noscammerspls";
 
-            const data = {
-                apiKey: API_KEY,
-                exercise: form.exercise.value,
-                sets: form.sets.value,
-                reps: form.reps.value,
-                weight: form.weight.value,
-            };
+if (form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-            fetch("https://script.google.com/macros/s/AKfycbwwz4uIaESy-WYhYVPuabcdGn9OYN1ek6FGIU0DLZ7ATp218sULf4RIqSUjVS6_0mewCA/exec", {
-                method: "POST",
-                body: JSON.stringify(data),
-                headers: { "Content-Type": "application/json" },
-                mode: "no-cors"
-            })
-                .then((response) => response.text())
-                .then((data) => alert("Workout saved!"))
-                .catch((error) => console.error("Error:", error));
-        });
+        const data = {
+            apiKey: API_KEY,
+            exercise: form.exercise.value,
+            sets: form.sets.value,
+            reps: form.reps.value,
+            weight: form.weight.value,
+        };
+
+        fetch("https://script.google.com/macros/s/AKfycbwwz4uIaESy-WYhYVPuabcdGn9OYN1ek6FGIU0DLZ7ATp218sULf4RIqSUjVS6_0mewCA/exec", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+            mode: "no-cors"
+        })
+            .then((response) => response.text())
+            .then((data) => alert("Workout saved!"))
+            .catch((error) => console.error("Error:", error));
+    });
+}
