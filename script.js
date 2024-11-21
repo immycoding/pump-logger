2c69884ae770c84c90fdba84e173be20f4197eeb
immyj <johnsonimmy@gmail.com>
2024-11-20 20:57:57 -0600

    debug

--- script.js
+++ script.js
@@ -26,27 +26,28 @@ if (window.location.pathname.endsWith("workout.html")) {
             option.textContent = exercise;
             dropdown.appendChild(option);
         });
-const form = document.getElementById("workoutForm");
-const API_KEY = "noscammerspls";
+    }}
+        const form = document.getElementById("workoutForm");
+        const API_KEY = "noscammerspls";
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
 
-    const data = {
-        apiKey: API_KEY,
-        exercise: form.exercise.value,
-        sets: form.sets.value,
-        reps: form.reps.value,
-        weight: form.weight.value,
-    };
+            const data = {
+                apiKey: API_KEY,
+                exercise: form.exercise.value,
+                sets: form.sets.value,
+                reps: form.reps.value,
+                weight: form.weight.value,
+            };
 
-    fetch("https://script.google.com/macros/s/AKfycbwwz4uIaESy-WYhYVPuabcdGn9OYN1ek6FGIU0DLZ7ATp218sULf4RIqSUjVS6_0mewCA/exec", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-        mode: "no-cors"
-    })
-        .then((response) => response.text())
-        .then((data) => alert("Workout saved!"))
-        .catch((error) => console.error("Error:", error));
-});
+            fetch("https://script.google.com/macros/s/AKfycbwwz4uIaESy-WYhYVPuabcdGn9OYN1ek6FGIU0DLZ7ATp218sULf4RIqSUjVS6_0mewCA/exec", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: { "Content-Type": "application/json" },
+                mode: "no-cors"
+            })
+                .then((response) => response.text())
+                .then((data) => alert("Workout saved!"))
+                .catch((error) => console.error("Error:", error));
+        });
